90f943f43fbe6973c71aed55623309aef855672c
Geraldine C <107955441+geraldinedepaul17@users.noreply.github.com>
2022-07-14 17:56:25 -0500

    first commit being made

---
+++ public class Main { BooleanConditionalsl.java
@@ -0,0 +1,16 @@
+public class Main { BooleanConditionalsloops{
+
+    pulbic static void main(String(} args) {
+
+        //create a variable named age and assign it a value of 14 
+        int age = 14
+
+        //print the boolean expression age >= 16 to the console and note the results. 
+        // change the value of age to 18 and run again. 
+        System.out.print(age >=16); 
+
+        
+    })
+}
+    
+}
